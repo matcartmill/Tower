94c7642d5001bd65579447b9a320b690027bb55d
Mat Cartmill <mat.cartmill@gmail.com>
2022-08-14 12:22:16 -0400

    Initial Commit

---
+++ Tower/TowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TowerApp.swift
+//  Tower
+//
+//  Created by Mat Cartmill on 2022-08-14.
+//
+
+import SwiftUI
+
+@main
+struct TowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
